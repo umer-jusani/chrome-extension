42e6d1bd30fa7148510115198fa02d5380124cb2
Muhammad Affan <muhammadaffan9800@gmail.com>
2025-04-18 19:40:58 +0500

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
+  const isLogged = true;
 
   const onClick = async () => {
-    let [tab] = await chrome.tabs.query({ active: true })
+    let [tab] = await chrome.tabs.query({ active: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      files: ['contentScript.js']
-    })
-  }
+      files: ["contentScript.js"],
+    });
+  };
 
   return (
     <>
-      <h1>Jobbee</h1>
-      <button className="btn" onClick={onClick}>Start Applying Job</button>
+      <div>
+        <img
+          src={
+            "https://master.d337x6ro71a0no.amplifyapp.com/_next/static/media/headerIcon.774021f0.svg"
+          }
+          className="logo react"
+        />
+      </div>
+      <div className="card">
+        <div className="card-buttons">
+          {isLogged ? (
+            <button onClick={onClick}>Start Auto Applying</button>
+          ) : (
+            <>
+              <button onClick={onClick}>Login</button>
+              <button onClick={onClick}>Signup</button>
+            </>
+          )}
+        </div>
+        <p>Boost Your Resume With AI And Apply To Jobs In One Click!</p>
+      </div>
+      <a
+        href="https://master.d337x6ro71a0no.amplifyapp.com/"
+        className="read-the-docs"
+      >
+        Click here to learn more
+      </a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
